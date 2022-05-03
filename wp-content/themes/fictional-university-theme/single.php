62f617f17f014edbd9a439a8e2592985e73b92b2
Erik Berger <erik.berger12@hotmail.com>
2022-05-03 17:34:41 +0200

    wordpress university theme

---
+++ wp-content/themes/fictional-university-theme/single.php
@@ -0,0 +1,27 @@
+<!-- This is for Individual Blog Posts, only for posts not for single pages  -->
+<?php
+  get_header();
+
+  // Wordpress loop, it does something once for each item in a collection
+  while (have_posts()) {
+    // will keep track on which post we currently working with
+    the_post(); 
+    pageBanner();
+    ?>
+
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+      </div>
+
+      <div class="generic-content">
+        <?php the_content(); ?>
+      </div>
+
+    </div>
+
+
+  <?php }
+
+  get_footer();
+?>
